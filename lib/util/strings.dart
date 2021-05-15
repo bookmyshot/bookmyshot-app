21bb9a78f11a18fe6fe46d841710d38c0d510820
Urja Thakkar <urja.thakkar@quicko.com>
2021-05-15 16:26:23 +0530

    util

--- lib/util/strings.dart
+++ lib/util/strings.dart
@@ -3,5 +3,6 @@
 // - splash
 const String svgLogo = 'res/svg/splash/logo.svg';
 // - onboarding
-
+// - welcome
+const String checkmark = 'res/svg/onboarding/welcome/confirm_slots.svg';
 // - dashboard
